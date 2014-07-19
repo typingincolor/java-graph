ea0265c18366a29209552cf70f1ac52887a9d19c
Andrew Braithwaite <abraithw@gmail.com>
2014-07-19 13:17:54 +0100

    failing add edge test

--- src/test/java/com/losd/tw/GraphTest.java
+++ src/test/java/com/losd/tw/GraphTest.java
@@ -1,7 +1,15 @@
 package com.losd.tw;
 
+import org.junit.Test;
+
 /**
  * Created by andrew on 19/07/2014.
  */
 public class GraphTest {
+    @Test
+    public void addEdge() {
+        Graph graph = new Graph();
+
+        graph.addEdge("A", "B", "5");
+    }
 }
