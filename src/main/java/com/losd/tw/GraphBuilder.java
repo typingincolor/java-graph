4acec47f02871050ad7b66f788955e8e3f93dd3c
Andrew Braithwaite <abraithw@gmail.com>
2014-07-20 11:17:57 +0100

    replace all spaces in one go

--- src/main/java/com/losd/tw/GraphBuilder.java
+++ src/main/java/com/losd/tw/GraphBuilder.java
@@ -3,16 +3,18 @@ package com.losd.tw;
 import java.util.StringTokenizer;
 
 /**
+ * Builds graphs
+ *
  * Created by andrew on 20/07/2014.
  */
 public abstract class GraphBuilder {
     public static Graph build(String graph) {
 
-        StringTokenizer st = new StringTokenizer(graph, ",", false);
+        StringTokenizer st = new StringTokenizer(graph.replace(" ", ""), ",", false);
 
         Graph newGraph = new Graph();
         while (st.hasMoreTokens()) {
-            String token = st.nextToken().replace(" ", "");
+            String token = st.nextToken();
             Town a = Town.getInstance(Character.toString(token.charAt(0)));
             Town b = Town.getInstance(Character.toString(token.charAt(1)));
             int distance = Integer.parseInt(token.substring(2));
